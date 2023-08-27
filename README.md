436e173621d56eb601c2c76d6c5267597221f58f
avsk80 <avskkrish80@gmail.com>
2023-08-27 18:47:37 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# IPNB REnderer project
---
+++ a.py
@@ -0,0 +1,67 @@
+a
+
+# write a program to add two numbers in OOP
+
+class Addition:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.sum = a + b
+        print(self.sum)
+        return self.sum
+    def __add__(self, other):
+        return self.sum + other.sum
+    def __sub__(self, other):
+        return self.sum - other.sum
+    def __mul__(self, other):
+        def __mul__(self, other):
+        return self.sum * other.sum
+    def __truediv__(self, other):
+        return self.sum / other.sum
+    def __floordiv__(self, other):
+        return self.sum // other.sum
+    def __mod__(self, other):
+        return self.sum % other.sum
+    def __pow__(self, other):
+        return self.sum ** other.sum
+    def __lshift__(self, other):
+        return self.sum << other.sum
+    def __rshift__(self, other):
+        return self.sum >> other.sum
+    def __and__(self, other):
+        return self.sum & other.sum
+    def __xor__(self, other):
+        def __xor__(self, other):
+        return self.sum ^ other.sum
+    def __or__(self, other):
+        return self.sum | other.sum
+    def __radd__(self, other):
+        def __radd__(self, other):
+        return self.sum + other.sum
+    def __rsub__(self, other):
+        def __rsub__(self, other):
+        return self.sum - other.sum
+    def __rmul__(self, other):
+        def __rmul__(self, other):
+        return self.sum * other.sum
+    def __rtruediv__(self, other):
+        def __rtruediv__(self, other):
+        return self.sum / other.sum
+    def __rfloordiv__(self, other):
+        def __rfloordiv__(self, other):
+        return self.sum // other.sum
+    def __rmod__(self, other):
+        def __rmod__(self, other):
+        return self.sum % other.sum
+    def __rpow__(self, other):
+        def __rpow__(self, other):
+        def __rpow__(self, other):
+        return self.sum ** other.sum
+    def __rlshift__(self, other):
+        def __rlshift__(self, other):
+        def __rlshift__(self, other):
+        return self.sum << other.sum
+    
+
+
+
